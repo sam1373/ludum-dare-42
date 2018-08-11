e8ef849b2a35ea4b22e72eb91f1a3c5682e88117
sam1373 <samuelkriman@gmail.com>
2018-08-11 06:32:25 +0300

    first commit

---
+++ Assets/GameEngine.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEngine : MonoBehaviour {
+
+    public GameObject growthPrefab;
+
+    public int totalGrowth;
+
+    public float lastGrowthSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                   Input.mousePosition.y, Camera.main.nearClipPlane));
+        worldMousePos.z = 0;
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newGrowth = Instantiate(growthPrefab, worldMousePos, new Quaternion());
+            Color rndCol = new Color(Random.Range(0.7f, 1f), Random.Range(0.5f, 0.9f), Random.Range(0.3f, 0.7f), 1);
+            newGrowth.GetComponent<SpriteRenderer>().color = rndCol;
+            newGrowth.transform.localScale = new Vector3(0.01f, 0.01f, 1);
+            newGrowth.GetComponent<Growth>().maxSize = 3;
+            lastGrowthSpawn = 0;
+        }
+
+        totalGrowth = FindObjectsOfType<Growth>().Length;
+
+        lastGrowthSpawn += Time.deltaTime;
+
+        if(lastGrowthSpawn > 10 && totalGrowth < 50)
+        {
+            Vector3 rndVec = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+            rndVec.Normalize();
+            rndVec *= 6.8f;
+            GameObject newGrowth = Instantiate(growthPrefab, rndVec, new Quaternion());
+            Color rndCol = new Color(Random.Range(0.7f, 1f), Random.Range(0.5f, 0.9f), Random.Range(0.3f, 0.7f), 1);
+            newGrowth.GetComponent<SpriteRenderer>().color = rndCol;
+            newGrowth.transform.localScale = new Vector3(0.01f, 0.01f, 1);
+            newGrowth.GetComponent<Growth>().maxSize = 3;
+            lastGrowthSpawn = 0;
+        }
+    }
+}
---
+++ Assets/Growth.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Growth : MonoBehaviour {
+
+    GameEngine eng;
+
+    float nextSpawn;
+    public float maxSize;
+
+    int maxCreate;
+
+	// Use this for initialization
+	void Start () {
+        nextSpawn = 5;
+        eng = FindObjectOfType<GameEngine>();
+        //transform.localScale = new Vector3(0.01f, 0.01f, 1);
+        maxCreate = 1;
+
+        //print(transform.position.magnitude);
+        //if (transform.position.magnitude > 6)
+        //    Destroy(this.gameObject);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float td = Time.deltaTime;
+
+        float curSize = transform.localScale.x;
+        Vector3 globScale = transform.lossyScale;
+
+        //print(curSize);
+        if (curSize < maxSize)
+        {
+            transform.localScale = new Vector3(curSize + td * 0.1f, curSize + td * 0.1f);
+        }
+
+        if(curSize > 2)
+            nextSpawn -= td;
+
+        if (nextSpawn < 0 && maxCreate > 0 && eng.totalGrowth < 50)
+        {
+            Vector3 rndVec = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+            rndVec.Normalize();
+            rndVec = rndVec * globScale.x * Random.Range(0.4f, 0.5f);
+            if ((transform.position + rndVec).magnitude < 6)
+            {
+
+
+                GameObject newGrowth = Instantiate(eng.growthPrefab, transform.position + rndVec, new Quaternion());
+
+                newGrowth.transform.localScale = new Vector3(0.01f, 0.01f, 1);
+
+                //Color rndCol = new Color(Random.Range(0.7f, 1f), Random.Range(0.3f, 0.7f), Random.Range(0.3f, 0.7f), 1);
+                newGrowth.GetComponent<SpriteRenderer>().color = GetComponent<SpriteRenderer>().color;
+
+                newGrowth.GetComponent<Growth>().maxSize = Random.Range(1.5f, 3);
+
+                nextSpawn = 6;
+                maxCreate--;
+                eng.lastGrowthSpawn = 0;
+            }
+        }
+        
+	}
+}
